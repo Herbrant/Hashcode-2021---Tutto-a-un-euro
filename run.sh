79aa69ee0810fe299e30aa093478e127be21ca49
herbrant <cdavide98carnemolla@gmail.com>
2021-02-25 21:27:40 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-FILE=$1
 
-if [ -z "$FILE" ]; then
-	echo "USE: ./run.sh INPUT_FILE"
-else
-	python3 main.py "$FILE"
+python3 main.py a
+python3 main.py b
+python3 main.py c
+python3 main.py d
+python3 main.py e
+python3 main.py f
 
-	[ -f "code.zip" ] && rm code.zip
-	zip code.zip *.py
-fi
+
+[ -f "code.zip" ] && rm code.zip
+zip code.zip *.py
+
 
