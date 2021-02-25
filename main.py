c1d636cf6c05625ceefe49ffe3a118b271c3f048
herbrant <cdavide98carnemolla@gmail.com>
2021-02-25 18:18:29 +0100

    Add useful functions

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import os, sys
 
 def main():
-    print("Tutto a un euro")
+    read_input(open(sys.argv[1], "r"))
+
+    solve()
+    output("output_{}.txt".format(sys.argv[1]))
+
+    print(state.to_string())
+
+def output(filename):
+    outfile = open(filename, "w")
+
+    return
+
+def solve():
+
+    return
+
+def read_input(input_file):
+
+    return
 
 
 if __name__ == "__main__":
